6222b92dcda87282ba931b3c9622acc62f79ce22
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2024-07-05 00:29:30 +0600

    adding style and functionality to the app

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-const btn = document.getElementById('submit');
+const correctOption = document.getElementById('c');
+const btn = document.getElementById('submitBtn');
+const feedback = document.getElementById('feedback');
+
+btn.onclick = quiz;
+
+function quiz() {
+  if ((correctOption.innerText = 'c) 14')) {
+    console.log('ok');
+    feedback.innerHTML = 'ok';
+  } else {
+    console.log('not ok');
+    feedback.innerHTML = 'not ok';
+  }
+}
