8017a7c9b4d3e9c150c66dea1c2e1abe740b2bcf
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2024-07-05 09:47:39 +0600

    added the function that the users can't rechange theire choice after submiting (¬‿¬)

--- main.js
+++ main.js
@@ -13,6 +13,9 @@ function quiz() {
     feedback.style.display = 'block';
     feedback.style.fontWeight = 'bolder';
     feedback.innerHTML = 'Congrats! You Make A Correct Answer';
+    btn.onclick = () => {
+      return;
+    };
   } else {
     console.log('not ok');
     feedback.innerHTML = 'Incorrect. the correct answer is c) 14';
@@ -20,5 +23,8 @@ function quiz() {
     feedback.style.color = 'red';
     feedback.style.fontWeight = 'bolder';
     feedback.style.background = 'wheat';
+    btn.onclick = () => {
+      return;
+    };
   }
 }
